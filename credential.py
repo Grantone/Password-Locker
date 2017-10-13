ec6113c0d4f953936d7308ac9090565d0de8bef5
Granton Mchana <gmchana254@gmail.com>
2017-10-13 14:29:39 +0300

    deletions and insertions

--- credential.py
+++ credential.py
@@ -1,11 +1,40 @@
-class credentials:
+import sys
+import random
+
+
+UserName: "Mchana"
+Password: "G1234"
+status: ""
+
+
+ans = True
+
+while ans:
+    question = input("Ask a question: (press q to quit)")
+
+    answers = random.randint(1, 5)
+
+    if question == "":
+        sys.exit()
+
+    elif:
+            answer == 1:
+        print("Please enter your name: ")
+
+    elif:
+            answer == 2:
+        print("Please enter account name: ")
+
+    elif:
+            answer== 3:
+        print("Enter Password")
 
     '''
     This class is for generating credentials
     '''
 
 
-password_list = []
+user_list = []
 
 
 def __init__(self, first_name, account_name, password,):
@@ -13,3 +42,6 @@ def __init__(self, first_name, account_name, password,):
     self.first_name = first_name
     self.account_name = account_name
     self.password = password
+
+
+user_details = []
--- password_test.py
+++ password_test.py
@@ -1,5 +1,4 @@
 import unittest
-from password import Password
 
 
 class TestPassword(unittest.TestCase):
