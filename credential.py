3c06e7eaec943e026df82b8ad012f8782355a03b
Granton Mchana <gmchana254@gmail.com>
2017-10-17 22:12:32 +0300

    insertions

--- credential.py
+++ credential.py
@@ -4,32 +4,32 @@ from credential import Credential
 
 class TestUser(unittest.TestCase):
 
-    def setUp(self):
+ def setUp(self):
         """
         Class that runs every new instances of credentials
         """
 
-    self.new_credent = Credential
-    ('Mchana', 'q1234', 'email')
+       self.new_credent=Credential
+       ('Mchana', 'q1234', 'email')
 
-        credential_list = []  # Empty contact list
-        # Init method up here
+       credential_list = []  # Empty contact list
+       # Init method up here
 
-        def setUp(self):
-            '''
-            to set up all the credentials as created
-            '''
+ def setUp(self):
+        '''
+        to set up all the credentials as created
+        '''
 
-            self.assertEqual(self.new_credent.username, 'Mchana')
-            self.assertEqual(self.new_credent.password, 'q1234')
-            self.assertEqual(self.new_credent.account, 'email')
+        self.assertEqual(self.new_credent.username, 'Mchana')
+        self.assertEqual(self.new_credent.password, 'q1234')
+        self.assertEqual(self.new_credent.account, 'email')
 
-        def test_save(self):
-            '''
-            Test to save the credentials created for each user
-            '''
-            self.new_credent.save_credential()
-            self.assertEqual(len(Credential.credent_list), 1)
+ def test_save(self):
+        '''
+        Test to save the credentials created for each user
+        '''
+        self.new_credent.save_credential()
+        self.assertEqual(len(Credential.credent_list), 1)
 
 
 if __name__ == '__main__':
