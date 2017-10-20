f0c69a847d1f8e203529d6e1177268c6fb1e550f
Granton Mchana <gmchana254@gmail.com>
2017-10-20 14:42:28 +0300

    insertions

--- credential.py
+++ credential.py
@@ -1,36 +1,21 @@
-import unittest
-from credential import Credential
+class Credential:
 
+    """
+    class that creates a new credential(cred)
+    """
+    credent_list=[]
 
-class TestUser(unittest.TestCase):
+def __init__(self,username,password,account):
+    '''
+    define the properties of the class
+    '''
+    self.username=username
+    self.password=password
+    self.account=account
 
- def setUp(self):
-        """
-        Class that runs every new instances of credentials
-        """
-
-       self.new_credent=Credential
-       ('Mchana', 'q1234', 'email')
-
-       credential_list = []  # Empty contact list
-       # Init method up here
-
- def setUp(self):
-        '''
-        to set up all the credentials as created
-        '''
-
-        self.assertEqual(self.new_credent.username, 'Mchana')
-        self.assertEqual(self.new_credent.password, 'q1234')
-        self.assertEqual(self.new_credent.account, 'email')
-
- def test_save(self):
-        '''
-        Test to save the credentials created for each user
-        '''
-        self.new_credent.save_credential()
-        self.assertEqual(len(Credential.credent_list), 1)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def save_credential(self):
+    '''
+    method to save the credentials inputted
+    '''
+    Credential.credent_list.append(self)
+    return self.credent_list
