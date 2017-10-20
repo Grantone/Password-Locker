5a8d4f2e9958fe7ce65494b66cd84f593d503dd3
Granton Mchana <gmchana254@gmail.com>
2017-10-20 14:33:39 +0300

    additions

---
+++ credential_test.py
@@ -0,0 +1,30 @@
+import unittest
+from credential import Credential
+
+class TestUser(unittest.TestCase):
+    '''
+    Class to test the functionality of the credentials
+    '''
+    def setUp(self):
+        '''
+        setUp method to test if the creentials run
+        '''
+        self.new_credent=Credential
+        ('Mchana', 'q1234', 'email')
+
+        def test_init(self):
+            '''
+            credentials initialized
+            '''
+            self.assertEqual(self.new_credent.username,'Mchana')
+            self.assertEqual(self.new_credent.password,'q1234')
+            self.assertEqual(self.new_credent.account,'email')
+def test_save(self):
+    '''
+    class for saving credentials
+    '''
+    self.new_credent.save_credential()
+    self.assertEqual(len(Credential.credent_list),1)
+
+    if __name__=='__main__':
+        unittest.main()
